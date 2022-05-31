5267e7eba03da5940187007f30c2016f0826727b
Siddharth Batra <sbsiddharth@gmail.com>
2022-05-31 12:23:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const cred = urlParams.get("cred");
 if(cred) {
     document.cookie = "cred="+cred;
 }
-if(document.cookie == "") {
-    document.getElementById("cookie_info").innerHTML = "No Cookie Found";
-} else {
-    document.getElementById("cookie_info").innerHTML = document.cookie;
-}
+// if(document.cookie == "") {
+//     document.getElementById("cookie_info").innerHTML = "No Cookie Found";
+// } else {
+//     document.getElementById("cookie_info").innerHTML = document.cookie;
+// }
 console.log("cookie stored");
 
 const sleep = (time) => {
@@ -16,8 +16,11 @@ const sleep = (time) => {
         setTimeout(resolve, time);
     });
 }
-sleep(2000);
+sleep(2000).then(() => {
+    document.getElementById("spinner").style.display = "none";
+    document.getElementById("success").style.display = "flex";
+});
 
-document.getElementsByClassName("loader").style.visibility = "hidden";
-
-window.location = "com.example.chromecustomtabexperiment://merry-arithmetic-d4fb40.netlify.app/openApp";
+const onClickHandler = () => {
+    window.location = "com.example.chromecustomtabexperiment://merry-arithmetic-d4fb40.netlify.app/openApp";
+}
