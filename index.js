c813bc72dc544db90dcd9ad78e9583c228003aa8
Siddharth Batra <sbsiddharth@gmail.com>
2022-06-01 11:34:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const cred = urlParams.get("cred");
+
 if(cred) {
-    document.cookie = "cred="+cred;
+    window.localStorage.setItem('cred', cred);
 }
-// if(document.cookie == "") {
-//     document.getElementById("cookie_info").innerHTML = "No Cookie Found";
-// } else {
-//     document.getElementById("cookie_info").innerHTML = document.cookie;
-// }
+if(window.localStorage.setItem('cred', cred) == null) {
+    document.getElementById("cookie_info").innerHTML = "No LS Found";
+} else {
+    document.getElementById("cookie_info").innerHTML = window.localStorage.setItem('cred', cred);;
+}
 console.log("cookie stored");
 
 const sleep = (time) => {
